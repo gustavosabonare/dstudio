b6cf6362083919baba2e71bb4769b384423eebd6
Gustavo Sabonare <gustavosabonare@hotmail.com>
2020-01-19 19:37:16 -0300

    new footer

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -1,25 +1,28 @@
 import React, { Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+// Components
+import Footer from './components/footer';
+
 // Containers
 import HeaderContainer from './containers/header';
 import PlayerContainer from './containers/player';
 
 // Scenes
-import {
-  Event,
-  Page,
-} from './scenes';
+import { Event, Page } from './scenes';
 
 import "./app.scss";
 
-export default (props) => (
+export default () => (
   <Fragment>
     <HeaderContainer />
-    <PlayerContainer />
+
     <Switch>
       <Route path='/details/:id' component={Event} exact />
       <Route path='/:page?' component={Page} />
     </Switch>
+
+    <PlayerContainer />
+    <Footer />
   </Fragment>
 );
