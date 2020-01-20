e183ef4f390f08b11bb9fd54845062968400bdae
Gustavo Sabonare <gustavosabonare@hotmail.com>
2020-01-19 22:19:15 -0300

    fix on tagging and favicon

--- src/client/components/tagging/index.jsx
+++ src/client/components/tagging/index.jsx
@@ -1,14 +1,15 @@
-/* globals gtag */
+/* globals gtag, window */
 
 import { useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 
 const Tagging = ({ history }) => {
-  useEffect(() => history.listen(() => {
+  useEffect(() => history.listen(() => setTimeout(() => {
     gtag('config', 'UA-156455344-1', {
-      page_path: history.location.pathname
+      page_path: history.location.pathname,
+      page_title: window.document.title,
     });
-  }))
+  }, 1000)))
 
   return null;
 }
--- src/server/pageTemplate.js
+++ src/server/pageTemplate.js
@@ -22,6 +22,7 @@ export default (html, helmet, preloadedState) => {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="shortcut icon" type="image/png" href="https://dstudio-storage.s3.sa-east-1.amazonaws.com/favicon.png"/>
         <link href="https://fonts.googleapis.com/css?family=Dosis:400,700" rel="stylesheet">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
             integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
